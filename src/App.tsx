56d3efcc47c2834fc39b62998ceab48588691f9c
Luan Fonseca <luanfonsecap@icloud.com>
2021-05-22 22:35:33 -0300

    add theme to chakra provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
+import theme from './styles/theme';
 import AppProvider from './hooks';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <>
     <AppProvider>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </ChakraProvider>
     </AppProvider>
   </>
 );
