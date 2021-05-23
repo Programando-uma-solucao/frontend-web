4be45afb2fd2a5538bbdf99d8dde2734cb1187ec
Luan Fonseca <luanfonsecap@icloud.com>
2021-05-22 23:17:26 -0300

    created login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import theme from './styles/theme';
+import Toast from './components/Toast';
 import AppProvider from './hooks';
 import Routes from './routes';
 
@@ -15,6 +16,7 @@ const App: React.FC = () => (
         </BrowserRouter>
       </ChakraProvider>
     </AppProvider>
+    <Toast />
   </>
 );
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,12 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
   </Switch>
 );
 
