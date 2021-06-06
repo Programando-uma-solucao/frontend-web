375a8372d84830788c98ae72a2370f5212b6f575
Luan Fonseca <luanfonsecap@icloud.com>
2021-06-06 14:27:40 -0300

    created page to ask a question

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ import ForgetPassword from '../pages/ForgetPassword';
 import AnswerSecretQuestion from '../pages/AnswerSecretQuestion';
 import RecoverPassword from '../pages/RecoverPassword';
 import RegisterUser from '../pages/RegisterUser';
+import AskQuestion from '../pages/AskQuestion';
 
 const Routes = () => (
   <Switch>
@@ -17,6 +18,7 @@ const Routes = () => (
     <Route path="/secret-question" exact component={AnswerSecretQuestion} />
     <Route path="/recover-password" exact component={RecoverPassword} />
     <Route path="/register" exact component={RegisterUser} />
+    <Route path="/ask-question" exact component={AskQuestion} />
   </Switch>
 );
 
