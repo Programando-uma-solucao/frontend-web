094dee6e88abe190ed4bebc680341081a7bf5be4
Luan Fonseca <luanfonsecap@icloud.com>
2021-06-07 21:17:54 -0300

    adjust space between texts in landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,12 @@ import Logo from '../assets/icon.svg';
 
 const Home = () => {
   return (
-    <Flex direction="column" justifyContent="center" alignItems="center">
+    <Flex
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      paddingX={5}
+    >
       <Box>
         <Text
           fontFamily="Merienda"
@@ -19,21 +24,30 @@ const Home = () => {
         <Image
           src={Logo}
           alt="Programando uma Solução Logo"
-          width="150"
-          height="150"
+          width="130"
+          height="130"
           marginX="auto"
+          mb={2}
         />
 
-        <Text textAlign="center" padding="5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt rem
-          harum molestiae facere impedit totam ad? Libero deleniti quae aut.
-          Aliquid ex voluptates vel pariatur! Impedit fuga voluptatibus corporis
-          iure.
+        <Text textAlign="center" paddingY="1">
+          Desenvolvido pelos alunos do projeto de extensão “Programando uma
+          solução”, do Centro Universitário UNA Betim, o Facilita UNA é uma
+          aplicação que visa fornecer soluções acessíveis e gratuitas para seus
+          usuários.
         </Text>
 
-        <Text textAlign="center" padding="5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt rem
-          harum molestiae facere impedit totam ad? Libero deleniti quae aut.
+        <Text textAlign="center" paddingY="1">
+          Como principal ferramenta temos a aproximação de pessoas em situação
+          de vulnerabilidade econômica com advogados, para que aqueles possam
+          expor suas dúvidas jurídicas e tem um aconselhamento profissional, de
+          maneira gratuita.
+        </Text>
+
+        <Text textAlign="center" paddingY="1">
+          Mais novidades serão incluídas nos próximos semestres, tornando a
+          aplicação cada vez mais completa funcional. Nos ajude a divulgar nosso
+          trabalho, apresente a aplicação para seus conhecidos.
         </Text>
       </Box>
     </Flex>
